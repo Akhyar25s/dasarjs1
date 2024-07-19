6432bb6bbbfc9937deb4ef75c119cc92d7c04d7e
Akhyar25s <akhyarrr25@gmail.com>
2024-07-20 01:43:08 +0800

    nyoba javascript

---
+++ dasar.js
@@ -0,0 +1,47 @@
+//cara membuat variabel
+var a = 'a';
+let b = 'b';
+const c = 'c'
+
+//variabel bisa menampung tipe data string, integrer/number, boolean, array, object
+
+let nama = "Akhyar"; //string
+let umur = 19; //number
+let apakahBenar = true; //boolean
+let uang = 4.330; //number namun ada komanya
+
+//operator aritmatika
+
+let angka1 = 10;
+let angka2 = 20;
+console.log(angka1 + angka2);
+console.log(angka1 - angka2);
+console.log(angka1 * angka2);
+console.log(angka1 / angka2);
+console.log(angka1 % angka2);
+
+let penampungHasilOperatorAritmatika = angka1 + angka2;
+console.log(penampungHasilOperatorAritmatika);
+penampungHasilOperatorAritmatika = angka1 - angka2;
+console.log(penampungHasilOperatorAritmatika);
+penampungHasilOperatorAritmatika = angka1 * angka2;
+console.log(penampungHasilOperatorAritmatika);
+penampungHasilOperatorAritmatika = angka1 / angka2;
+console.log(penampungHasilOperatorAritmatika);
+penampungHasilOperatorAritmatika = angka1 % angka2;
+console.log(penampungHasilOperatorAritmatika);
+
+//nama
+let firstname = "Muhammad";
+let lastname = "Akhyar";
+let fullname = firstname + ' ' + lastname;
+console.log(fullname);
+console.log(firstname + ' ' + lastname);
+console.log(firstname - lastname);
+
+//alur pembacaan kode
+
+let number = 30;
+console.log(number + 20);
+number = 20;
+console.log(number + 20);
